76cf98ee2dbe6b24377543815f79e3be5082ad1b
Luis Almeida <anjos.luiseduardo@gmail.com>
2023-09-08 17:22:45 -0300

    spring security configuration

--- src/main/java/com/example/sales/configuration/SecurityConfig.java
+++ src/main/java/com/example/sales/configuration/SecurityConfig.java
@@ -2,12 +2,18 @@ package com.example.sales.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
- //todo
-    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/swagger/**", "/h2-console/**", "/user/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+        return http.build();
     }
 }
